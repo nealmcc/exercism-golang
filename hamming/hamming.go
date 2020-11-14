002a836a9d1a37cc512bb631c0a85adede27658c
Neal McConachie <neal@mcconachie.co>
2020-11-14 21:47:13 +0000

    ex3: done

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,4 +1,19 @@
+// Package hamming holds exercise 2 from Exercism
 package hamming
 
+import "errors"
+
+// Distance calculates the Hamming Distance between two strings.
 func Distance(a, b string) (int, error) {
+	length := len(a)
+	if length != len(b) {
+		return 0, errors.New("Hamming distance is only defined for strings of equal length.")
+	}
+	dist := 0
+	for i := 0; i < length; i++ {
+		if a[i] != b[i] {
+			dist++
+		}
+	}
+	return dist, nil
 }
