b17ef8959eb8cd88e35658e58aa591ecd0050cd0
Neal McConachie <neal@mcconachie.co>
2020-11-14 21:34:18 +0000

    ex2: 5x faster, less memory

--- two-fer/two_fer.go
+++ two-fer/two_fer.go
@@ -1,11 +1,9 @@
 package twofer
 
-import "fmt"
-
 // ShareWith solves the 'two-fer' problem in Exercism.
 func ShareWith(name string) string {
 	if name == "" {
 		name = "you"
 	}
-	return fmt.Sprintf("One for %s, one for me.", name)
+	return "One for " + name + ", one for me."
 }
