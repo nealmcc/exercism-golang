bf0dccc18d023eeca668e6c8482f1e14e6ac8bd0
Neal McConachie <neal@mcconachie.co>
2020-11-15 14:17:03 +0000

    leap: done

--- leap/leap.go
+++ leap/leap.go
@@ -1,15 +1,15 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package leap should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package leap holds the solution for the 'leap year' exercise
 package leap
 
-// IsLeapYear should have a comment documenting it.
-func IsLeapYear(int) bool {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return false
+// IsLeapYear determines if the given year is a leap year
+func IsLeapYear(y int) bool {
+	if y%400 == 0 {
+		return true
+	}
+
+	if y%100 == 0 {
+		return false
+	}
+
+	return y%4 == 0
 }
