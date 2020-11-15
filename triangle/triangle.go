ac16ae6681770295a755874d78e599c686f6f0a2
Neal McConachie <neal@mcconachie.co>
2020-11-15 15:14:10 +0000

    triangle: faster performance
     - takes advantage of returning early during the isTriangle checks
       This makes it run over two times faster on my machine

--- triangle/triangle.go
+++ triangle/triangle.go
@@ -9,41 +9,33 @@ type Kind int
 const (
 	// Equ is a triangle with all three sides having the same length.
 	Equ Kind = 3
-
 	// Iso is a triagle with exactly two sides having the same length.
 	Iso Kind = 2
-
 	// Sca is a triangle with every side having different lengths.
 	Sca Kind = 1
-
 	// NaT is not a triangle.
 	NaT Kind = -1
 )
 
-// KindFromSides takes the lengths of the sides of a triangle and
-// determines what Kind it is.
+// KindFromSides finds the Kind of a potential triangle with sides of length a,b,c
 func KindFromSides(a, b, c float64) Kind {
 	if !isTriangle(a, b, c) {
 		return NaT
 	}
-
 	if isEquilateral(a, b, c) {
 		return Equ
 	}
-
 	if isScalene(a, b, c) {
 		return Sca
 	}
-
 	return Iso
 }
 
 func isTriangle(a, b, c float64) bool {
-	sidesPositive := a > 0 && b > 0 && c > 0
 	inf := math.Inf(1)
-	sidesFinite := a < inf && b < inf && c < inf
-	passesInequality := (b+c >= a) && (a+c >= b) && (a+b >= c)
-	return sidesPositive && sidesFinite && passesInequality
+	return a > 0 && b > 0 && c > 0 &&
+		a < inf && b < inf && c < inf &&
+		(b+c >= a) && (a+c >= b) && (a+b >= c)
 }
 
 func isEquilateral(a, b, c float64) bool {
