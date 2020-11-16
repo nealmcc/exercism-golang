aa74dd1b4a24b1df5a48cf4ea23c9a6c3e7ce190
Neal McConachie <neal@mcconachie.co>
2020-11-16 20:53:57 +0000

    rna: performance-optimised
     - adapt the 'oldnew' array approach from golang strings.Replacer struct
       but without the overhead of the strings library.
       So, we get 10% faster performance, while still only using 32 bytes
       instead of 64.

--- rna-transcription/rna_transcription.go
+++ rna-transcription/rna_transcription.go
@@ -1,14 +1,25 @@
 package strand
 
-import "strings"
+type replacer []byte
 
-var translation = strings.NewReplacer(
-	"C", "G",
-	"G", "C",
-	"T", "A",
-	"A", "U",
-)
+var oldnew = replacer{
+	'C', 'G',
+	'G', 'C',
+	'T', 'A',
+	'A', 'U',
+}
 
-func ToRNA(dna string) string {
-	return translation.Replace(dna)
+var maxK = len(oldnew) - 2
+
+func ToRNA(old string) string {
+	length := len(old)
+	new := make([]byte, length)
+	for i := 0; i < length; i++ {
+		for k := 0; k <= maxK; k += 2 {
+			if old[i] == oldnew[k] {
+				new[i] = oldnew[k+1]
+			}
+		}
+	}
+	return string(new)
 }
