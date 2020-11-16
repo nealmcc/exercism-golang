05c846997b6aece841b4db6b740e79e21fc34b8e
Neal McConachie <neal@mcconachie.co>
2020-11-16 20:01:19 +0000

    rna: discovered the 'Replacer'
     - this is 2x faster, using 2x the memory

--- rna-transcription/rna_transcription.go
+++ rna-transcription/rna_transcription.go
@@ -1,17 +1,14 @@
 package strand
 
-var translate = map[byte]byte{
-	'C': 'G',
-	'G': 'C',
-	'T': 'A',
-	'A': 'U',
-}
+import "strings"
+
+var translation = strings.NewReplacer(
+	"C", "G",
+	"G", "C",
+	"T", "A",
+	"A", "U",
+)
 
 func ToRNA(dna string) string {
-	length := len(dna)
-	rna := make([]byte, length)
-	for i := 0; i < length; i++ {
-		rna[i] = translate[dna[i]]
-	}
-	return string(rna)
+	return translation.Replace(dna)
 }
