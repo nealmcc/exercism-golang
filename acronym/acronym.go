b5f8abaafee85706dec075dabf24a44fdd42af23
Neal McConachie <neal@mcconachie.co>
2020-11-18 17:43:57 +0000

    acronym: by hand
     - still works, but with 4000 ns/op, 1088 B/op and 71 alloc/op
     - also, I had fun learning how to use 'lambda expressions' in Go

--- acronym/acronym.go
+++ acronym/acronym.go
@@ -1,20 +1,44 @@
 // Package holds a solution to the Exercism side-exercise of the same name
 package acronym
 
-import (
-	"regexp"
-	"strings"
-)
-
-var re = regexp.MustCompile(`[a-zA-Z']+`)
+import "strings"
 
 // Abbreviate creates an acronym for the given string
 // using the first letter of each word
 func Abbreviate(s string) string {
-	words := re.FindAllString(s, -1)
-	acronym := make([]byte, len(words))
-	for i, word := range words {
-		acronym[i] = word[0]
+	phrase := TrimUntilMatch([]byte(s), IsWordPart)
+	var acronym []byte
+	for len(phrase) > 0 {
+		acronym = append(acronym, phrase[0])
+		phrase = TrimUntilMatch(phrase, not(IsWordPart))
+		phrase = TrimUntilMatch(phrase, IsWordPart)
 	}
 	return strings.ToUpper(string(acronym))
+}
+
+func IsWordPart(b byte) bool {
+	return 'A' <= b && b <= 'Z' ||
+		'a' <= b && b <= 'z' ||
+		'\'' == b
+}
+
+func not(m matcher) matcher {
+	return func(b byte) bool {
+		return !m(b)
+	}
+}
+
+type matcher func(byte) bool
+
+func TrimUntilMatch(s []byte, isMatch matcher) []byte {
+	i, length := 0, len(s)
+	found := false
+	for i < length && !found {
+		found = isMatch(s[i])
+		i++
+	}
+	if found {
+		return s[i-1:]
+	}
+	return []byte{}
 }
