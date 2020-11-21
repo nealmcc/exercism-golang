93f9f95b9f8a542772f164b23e0042d30d4efe23
Neal McConachie <neal@mcconachie.co>
2020-11-21 18:26:15 +0000

    list-ops: done

--- list-ops/list_ops.go
+++ list-ops/list_ops.go
@@ -8,41 +8,70 @@ type predFunc func(int) bool
 type unaryFunc func(int) int
 
 // Append adds all items in the given list to this list
-func (left IntList) Append(right IntList) IntList {
-	return left
+func (self IntList) Append(other IntList) IntList {
+	for _, val := range other {
+		self = append(self, val)
+	}
+	return self
 }
 
 // Concat appends all items from all of the given lists to this list
-func (left IntList) Concat(right []IntList) IntList {
-	return left
+func (self IntList) Concat(lists []IntList) IntList {
+	for _, other := range lists {
+		self = self.Append(other)
+	}
+	return self
 }
 
 // Filter returns a new list of all items from this list which match the predicate
-func (left IntList) Filter(f predFunc) IntList {
-	return left
+func (self IntList) Filter(f predFunc) IntList {
+	filtered := IntList{}
+	for _, val := range self {
+		if f(val) {
+			filtered = append(filtered, val)
+		}
+	}
+	return filtered
 }
 
 // Length returns the number of items in this list.
-func (left IntList) Length() int {
-	return len(left)
+func (self IntList) Length() int {
+	return len(self)
 }
 
 // Map appiles the given function to each item in this list, returning the list of results
-func (left IntList) Map(f unaryFunc) IntList {
-	return left
+func (self IntList) Map(f unaryFunc) IntList {
+	mapped := make(IntList, len(self))
+	for i, val := range self {
+		mapped[i] = f(val)
+	}
+	return mapped
 }
 
-// Foldl accumulates the items, using the given function and starting value, starting from the left (index 0)
-func (left IntList) Foldl(f binFunc, start int) int {
-	return start
+// Foldl accumulates the items, using the given function and starting value, starting from the self (index 0)
+func (self IntList) Foldl(f binFunc, n int) int {
+	for _, val := range self {
+		n = f(n, val)
+	}
+	return n
 }
 
 // Foldr accumulates the items, using the given function and starting value, starting from the right (index Length()-1)
-func (left IntList) Foldr(f binFunc, start int) int {
-	return start
+func (self IntList) Foldr(f binFunc, n int) int {
+	for i := len(self) - 1; i >= 0; i-- {
+		n = f(self[i], n)
+	}
+	return n
 }
 
 // Reverse returns a new list of all items in reverse order.
-func (left IntList) Reverse() IntList {
-	return left
+func (self IntList) Reverse() IntList {
+	length := len(self)
+	reversed := make(IntList, length)
+	k := length
+	for i := 0; i < length; i++ {
+		k--
+		reversed[k] = self[i]
+	}
+	return reversed
 }
