577541e96e198fcd6359099bd309a153ec7e2124
Neal McConachie <neal@mcconachie.co>
2020-11-21 17:32:03 +0000

    collatz: done

--- collatz-conjecture/collatz_conjecture.go
+++ collatz-conjecture/collatz_conjecture.go
@@ -1,1 +1,21 @@
+// package collatzconjecture solves the side-exercise from Exercism
 package collatzconjecture
+
+import "errors"
+
+// CollatzConjecture counts the number of steps to solve the '3x+1 problem'
+func CollatzConjecture(n int) (int, error) {
+	if n < 1 {
+		return 0, errors.New("Input must be positive.")
+	}
+	i := 0
+	for n > 1 {
+		if n%2 == 0 {
+			n = n / 2
+		} else {
+			n = 3*n + 1
+		}
+		i++
+	}
+	return i, nil
+}
