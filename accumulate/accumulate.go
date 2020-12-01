fd3227ce736e9bbb07a1dd7a158c93aa327f3552
Neal McConachie <neal@mcconachie.co>
2020-12-01 19:32:52 +0000

    accumulate: done

--- accumulate/accumulate.go
+++ accumulate/accumulate.go
@@ -1,1 +1,13 @@
+// Package accumulate solves the Exercism Accumulate side exercise
 package accumulate
+
+type operation func(string) string
+
+// Accumulate transforms each element of the input using the given operation
+func Accumulate(in []string, op operation) []string {
+	out := make([]string, len(in))
+	for i, s := range in {
+		out[i] = op(s)
+	}
+	return out
+}
