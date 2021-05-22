b67b98df0d8e8cd69e59e353fc1ca9b667661818
Neal McConachie <neal@mcconachie.co>
2021-05-22 20:49:54 +0100

    etl: done

--- etl/etl.go
+++ etl/etl.go
@@ -1,1 +1,15 @@
 package etl
+
+func Transform(in map[int][]string) map[string]int {
+	out := map[string]int{}
+	for score, letters := range in {
+		for _, s := range letters {
+			ch := []byte(s)
+			if 'a' > []byte(ch)[0] {
+				ch[0] += 'a' - 'A'
+			}
+			out[string(ch)] = score
+		}
+	}
+	return out
+}
