2772073de2f232f219712dde2e29a137cddf5763
Neal McConachie <neal@mcconachie.co>
2021-05-22 19:57:11 +0100

    typo fixup

--- roman-numerals/roman_numerals.go
+++ roman-numerals/roman_numerals.go
@@ -19,7 +19,7 @@ func ToRomanNumeral(n int) (string, error) {
 
 var (
 	ones      = []string{"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"}
-	tens      = []string{"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXX", "XC"}
+	tens      = []string{"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"}
 	hundreds  = []string{"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"}
 	thousands = []string{"", "M", "MM", "MMM"}
 )
