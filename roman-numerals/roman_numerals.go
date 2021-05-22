3c72eea077755885e453e65292c99348669729fc
Neal McConachie <neal@mcconachie.co>
2021-05-22 19:55:42 +0100

    slice instead of map - almost 2x faster

--- roman-numerals/roman_numerals.go
+++ roman-numerals/roman_numerals.go
@@ -18,51 +18,10 @@ func ToRomanNumeral(n int) (string, error) {
 }
 
 var (
-	ones = map[int]string{
-		0: "",
-		1: "I",
-		2: "II",
-		3: "III",
-		4: "IV",
-		5: "V",
-		6: "VI",
-		7: "VII",
-		8: "VIII",
-		9: "IX",
-	}
-
-	tens = map[int]string{
-		0: "",
-		1: "X",
-		2: "XX",
-		3: "XXX",
-		4: "XL",
-		5: "L",
-		6: "LX",
-		7: "LXX",
-		8: "LXX",
-		9: "XC",
-	}
-
-	hundreds = map[int]string{
-		0: "",
-		1: "C",
-		2: "CC",
-		3: "CCC",
-		4: "CD",
-		5: "D",
-		6: "DC",
-		7: "DCC",
-		8: "DCCC",
-		9: "CM",
-	}
-
-	thousands = map[int]string{
-		0: "",
-		1: "M",
-		2: "MM",
-		3: "MMM",
-	}
-
-	ErrOutOfBounds = errors.New("decimal must be between 1 and 3000")
+	ones      = []string{"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"}
+	tens      = []string{"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXX", "XC"}
+	hundreds  = []string{"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"}
+	thousands = []string{"", "M", "MM", "MMM"}
 )
+
+var ErrOutOfBounds = errors.New("decimal must be between 1 and 3000")
