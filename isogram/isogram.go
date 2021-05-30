d113f954b1f69ef25a7a4064a713c454f6e6eb00
Neal McConachie <neal@mcconachie.co>
2021-05-30 19:51:22 +0100

    isogram: done

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -1,1 +1,15 @@
 package isogram
+
+import "strings"
+
+func IsIsogram(s string) bool {
+	found := make(map[rune]bool, len(s))
+	s = strings.ToLower(s)
+	for _, r := range s {
+		if found[r] && r != ' ' && r != '-' {
+			return false
+		}
+		found[r] = true
+	}
+	return true
+}
