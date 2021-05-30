5a5d996f4f33b690d710627b8091b0040a39c482
Neal McConachie <neal@mcconachie.co>
2021-05-30 21:32:18 +0100

    reverse: done
     - possibly not very efficient, but effective and simple

--- reverse-string/reverse_string.go
+++ reverse-string/reverse_string.go
@@ -1,1 +1,12 @@
 package reverse
+
+// Reverse returns the reverse of the given string
+func Reverse(s string) string {
+	runes := []rune(s)
+	length := len(runes)
+	rev := make([]rune, length)
+	for i, r := range runes {
+		rev[length-i-1] = r
+	}
+	return string(rev)
+}
