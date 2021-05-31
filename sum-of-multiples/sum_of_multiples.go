f10ded6a87072f015e81ead0a771cf3177415f58
Neal McConachie <neal@mcconachie.co>
2021-05-31 20:57:48 +0100

    working, but slow

--- sum-of-multiples/sum_of_multiples.go
+++ sum-of-multiples/sum_of_multiples.go
@@ -1,1 +1,24 @@
 package summultiples
+
+func SumMultiples(limit int, divisors ...int) int {
+	sum := 0
+	for n := 1; n < limit; n++ {
+		if isMultiple(n, divisors) {
+			sum += n
+		}
+	}
+	return sum
+}
+
+func isMultiple(n int, divisors []int) bool {
+	for _, d := range divisors {
+		if d == 0 {
+			// ignore 0 as a divisor
+			continue
+		}
+		if n%d == 0 {
+			return true
+		}
+	}
+	return false
+}
