4ab4ce318674e39fb2124a94de48467bd8741422
Neal McConachie <neal@mcconachie.co>
2021-05-31 13:28:06 +0100

    Revert "isogram: prevent 'DoS' via large strings"
    This reverts commit 33c4a45645d5952e7cbe23355c9aa493c56f76ca.

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -1,56 +1,15 @@
 package isogram
 
-import (
-	"bufio"
-	"io"
-	"strings"
-)
+import "strings"
 
-// IsIsogram determines if the given string contains at most one of each letter
-// in the english alphabet. Any other symbol or character is ignored.
-// This function just exists to facilitate the tests from the exercise.
 func IsIsogram(s string) bool {
-	isIso, err := IsIso(strings.NewReader(s))
-	if err != nil {
-		return false
+	found := make(map[rune]bool, len(s))
+	s = strings.ToLower(s)
+	for _, r := range s {
+		if found[r] && r != ' ' && r != '-' {
+			return false
+		}
+		found[r] = true
 	}
-	return isIso
-}
-
-// IsIso determines if any character in the english alphabet appears more than
-// once in the stream. Characters other than a-z and A-Z are ignored.
-// I've written this as a stream handler, to simulate the situation where we're
-// providing some public API that needs to be resistant to denial of service
-// attacks in the form of very large input.
-// This function has an upper limit on the amount of memory it can consume,
-// regardless of the amount of data on the stream.
-func IsIso(stream io.Reader) (bool, error) {
-	r := bufio.NewReader(stream)
-	// 0=a, 25=z
-	found := [26]bool{}
-	for {
-		rune, _, err := r.ReadRune()
-		if err != nil {
-			if err == io.EOF {
-				return true, nil
-			}
-			return false, err
-		}
-		if rune < 'A' ||
-			rune > 'Z' && rune < 'a' ||
-			rune > 'z' {
-			// skip any non-english letters
-			continue
-		}
-		var i int
-		if rune <= 'Z' {
-			i = int(rune - 'A')
-		} else {
-			i = int(rune - 'a')
-		}
-		if found[i] {
-			return false, nil
-		}
-		found[i] = true
-	}
+	return true
 }
