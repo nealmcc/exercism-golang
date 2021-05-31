e671797e1b519ed16e041d9f431efae3e0a0dc4b
Neal McConachie <neal@mcconachie.co>
2021-05-31 18:30:30 +0100

    squares: done

--- difference-of-squares/difference_of_squares.go
+++ difference-of-squares/difference_of_squares.go
@@ -1,1 +1,23 @@
 package diffsquares
+
+func SquareOfSum(n int) int {
+	// 1 + 2 + 3 + 4
+	// 4 + 3 + 2 + 1
+	// -------------
+	// 5 + 5 + 5 + 5
+	// == (n)(n+1)
+	// == 2x(sum of 1..4)
+	sum := n * (n + 1) / 2
+	sq := sum * sum
+	return sq
+}
+
+func SumOfSquares(n int) int {
+	// https://proofwiki.org/wiki/Sum_of_Sequence_of_Squares
+	sum := n * (n + 1) * (2*n + 1) / 6
+	return sum
+}
+
+func Difference(n int) int {
+	return SquareOfSum(n) - SumOfSquares(n)
+}
