781a04a7c2f6fb6ae52a9bd21d29812492c91bcb
Neal McConachie <neal@mcconachie.co>
2021-06-13 16:09:46 +0100

    Revert "atbash-cipher: Combine into one function"
    This reverts commit ef93a0a2f96d6f7fcca5b5d7bfba9a683daa6388.
    
    Instead of optimising for speed, I'm going to optimise for flexibility

--- atbash-cipher/atbash_cipher.go
+++ atbash-cipher/atbash_cipher.go
@@ -11,49 +11,73 @@ import (
 // Digits are not transformed.
 // the cipher text is divided into 5-byte chunks separated by spaces
 func Atbash(plain string) string {
-	text := []byte(plain)
+	d := data{[]byte(plain)}
+	return d.normalize().transpose().format(5)
+}
 
-	// import the plain text, transposing as we go:
+type data struct {
+	text []byte
+}
+
+// normalize removes any invalid characters, and converts letters to lowercase
+func (d data) normalize() data {
 	var curr, next int
-	for ; next < len(text); next++ {
-		ch := text[next]
+	for ; next < len(d.text); next++ {
+		ch := d.text[next]
 		switch {
 		case 'a' <= ch && ch <= 'z':
-			text[curr] = inverse[ch-'a']
+			d.text[curr] = ch
 			curr++
 		case '0' <= ch && ch <= '9':
-			text[curr] = ch
+			d.text[curr] = ch
 			curr++
 		case 'A' <= ch && ch <= 'Z':
-			text[curr] = inverse[ch-'A']
+			d.text[curr] = ch + 'a' - 'A'
 			curr++
 		}
 	}
-	text = text[:curr]
+	d.text = d.text[:curr]
+	return d
+}
 
-	const chunkSize = 5
+// transpose flips the letters of the text
+func (d data) transpose() data {
+	for i, char := range d.text {
+		if 'a' <= char && char <= 'z' {
+			d.text[i] = inverse[char-'a']
+		}
+	}
+	return d
+}
+
+var inverse = []byte{'z', 'y', 'x', 'w', 'v', 'u', 't', 's', 'r', 'q', 'p', 'o', 'n', 'm', 'l', 'k', 'j', 'i', 'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a'}
+
+// format returns the cipher text as a space-separated string with chunks
+// of the given size. If the size <= 0 then the cipher text is not separated.
+func (d data) format(size int) string {
+	if size <= 0 {
+		return string(d.text)
+	}
 
 	// allocate a string builder of sufficient size
 	b := new(strings.Builder)
-	b.Grow(len(text) + len(text)/chunkSize)
+	b.Grow(len(d.text) + len(d.text)/size)
 
 	// write each chunk to the string builder
-	for start, end := 0, 0; end < len(text); start = end {
-		end += chunkSize
+	for start, end := 0, 0; end < len(d.text); start = end {
+		end += size
 
 		// the last chunk could be shorter than 5 bytes
-		if end > len(text) {
-			end = len(text)
+		if end > len(d.text) {
+			end = len(d.text)
 		}
 
-		b.Write(text[start:end])
+		b.Write(d.text[start:end])
 
 		// add a space if we have at least one more chunk
-		if end < len(text) {
+		if end < len(d.text) {
 			b.WriteByte(' ')
 		}
 	}
 	return b.String()
 }
-
-var inverse = []byte{'z', 'y', 'x', 'w', 'v', 'u', 't', 's', 'r', 'q', 'p', 'o', 'n', 'm', 'l', 'k', 'j', 'i', 'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a'}
